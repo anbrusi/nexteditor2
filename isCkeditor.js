e36c68f6e3a013d64d22aaee76573c519a05d32e
Andreas Brunnschweiler <anbrusi@bluewin.ch>
2023-09-19 14:22:21 +0200

    intermediate commit after adding HorizontalLine

--- isCkeditor.js
+++ isCkeditor.js
@@ -2,26 +2,37 @@
 
 import { ClassicEditor as ClassicEditorBase } from '@ckeditor/ckeditor5-editor-classic';
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+// import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import { Bold, Italic, Underline, Strikethrough, Code, Subscript, Superscript } from '@ckeditor/ckeditor5-basic-styles';
 import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
 import { Heading } from '@ckeditor/ckeditor5-heading';
 import { Link } from '@ckeditor/ckeditor5-link';
 import { List } from '@ckeditor/ckeditor5-list';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { FindAndReplace} from '@ckeditor/ckeditor5-find-and-replace';
+import { Font } from '@ckeditor/ckeditor5-font';
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 ClassicEditor.builtinPlugins = [
     Essentials,
-    Autoformat,
+    // Autoformat,
     Bold,
     Italic,
+    Underline,
+    Strikethrough,
+    Code,
+    Subscript,
+    Superscript,
     BlockQuote,
     Heading,
     Link,
     List,
-    Paragraph
+    Paragraph,
+    FindAndReplace,
+    Font,
+    HorizontalLine
 ];
 
 ClassicEditor.defaultConfig = {
@@ -31,12 +42,27 @@ ClassicEditor.defaultConfig = {
             '|',
             'bold',
             'italic',
+            'underline',
+            'strikethrough',
+            'code',
+            'subscript',
+            'superscript',
+            '|',
             'link',
             'bulletedList',
             'numberedList',
             'blockQuote',
             'undo',
-            'redo'
+            'redo',
+            '|',
+            'findAndReplace',
+            '|',
+            'fontSize',
+            'fontFamily',
+            'fontColor',
+            'fontBackgroundColor',
+            '|',
+            'horizontalLine'
         ]
     },
     language: 'en'
